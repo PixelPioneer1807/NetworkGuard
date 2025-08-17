16c7d53218085e35d7727e5c0ad8083336423e64
PixelPioneer1807 <thisisshivam18@gmail.com>
2025-08-17 20:54:29 +0530

    DataTransformation Done

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from networkguard.components.data_ingestion import DataIngestion
 
 from networkguard.exceptionhandling.exception import NetworkSecurityException
 from networkguard.logging.logger import logging
-from networkguard.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networkguard.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networkguard.entity.config_entity import TrainingPipelineConfig
 import sys
 from networkguard.components.data_validation import DataValidation
+from networkguard.components.data_transformation import DataTransformation
+
 
 if __name__=='__main__':
     try:
@@ -23,5 +25,11 @@ if __name__=='__main__':
         logging.info("DataValidation Completed")
         print(data_validation_artifact)
         
+        logging.info("Data Transformation started")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("DataTransformation done")
+        print(data_transformation_artifact)
     except Exception as e:
            raise NetworkSecurityException(e,sys)
