dfc6788864f62d598277761ac2b3542dbb9de782
PixelPioneer1807 <thisisshivam18@gmail.com>
2025-08-17 19:27:09 +0530

    Data Validation Done

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from networkguard.components.data_ingestion import DataIngestion
 
 from networkguard.exceptionhandling.exception import NetworkSecurityException
 from networkguard.logging.logger import logging
-from networkguard.entity.config_entity import DataIngestionConfig
+from networkguard.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networkguard.entity.config_entity import TrainingPipelineConfig
 import sys
-
+from networkguard.components.data_validation import DataValidation
 
 if __name__=='__main__':
     try:
@@ -14,6 +14,14 @@ if __name__=='__main__':
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate Date Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("DataValidation Completed")
+        print(data_validation_artifact)
+        
     except Exception as e:
            raise NetworkSecurityException(e,sys)
