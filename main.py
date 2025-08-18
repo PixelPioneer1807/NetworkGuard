488fb9f784395f107c78587ca8429a000cdff7d5
PixelPioneer1807 <thisisshivam18@gmail.com>
2025-08-19 01:07:41 +0530

    Model evaluation using MlFlow done

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from networkguard.components.data_ingestion import DataIngestion
-
 from networkguard.exceptionhandling.exception import NetworkSecurityException
 from networkguard.logging.logger import logging
-from networkguard.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networkguard.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig, ModelTrainerConfig
 from networkguard.entity.config_entity import TrainingPipelineConfig
 import sys
 from networkguard.components.data_validation import DataValidation
 from networkguard.components.data_transformation import DataTransformation
+from networkguard.components.model_trainer import ModelTrainer
 
 
 if __name__=='__main__':
@@ -31,5 +31,13 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("DataTransformation done")
         print(data_transformation_artifact)
+        
+        logging.info("Model training Starred")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+        model_train_artifact=model_trainer.initiate_model_trainer()
+        
+        logging.info("Model Training artifact created")
+        
     except Exception as e:
            raise NetworkSecurityException(e,sys)
