e74f2927ec07624584caee454bad089ec07c7c58
PixelPioneer1807 <thisisshivam18@gmail.com>
2025-08-19 14:00:59 +0530

    Pipeline is completed

--- networkguard/components/model_trainer.py
+++ networkguard/components/model_trainer.py
@@ -27,6 +27,7 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
+dagshub.init(repo_owner='PixelPioneer1807', repo_name='NetworkGuard', mlflow=True)
 # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
 # os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
 # os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
@@ -39,7 +40,7 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def track_mlflow(self,best_model,classificationmetric):
+    def track_mlflow(self,best_model,classificationmetric, best_model_name="Model"):
         # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
@@ -55,15 +56,11 @@ class ModelTrainer:
             mlflow.sklearn.log_model(best_model,"model")
             # Model registry does not work with file store
             if tracking_url_type_store != "file":
-
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
-            else:
-                mlflow.sklearn.log_model(best_model, "model")
-
+                mlflow.sklearn.log_model(
+                    sk_model=best_model,
+                    artifact_path="model",
+                    registered_model_name=f"{best_model_name}_Model"
+                )
 
         
     def train_model(self,X_train,y_train,x_test,y_test):
